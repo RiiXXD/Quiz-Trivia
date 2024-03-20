4482f26dc0e98875e4556139a535f759a1b760ec
RiiXXD <thereforeme06@gmail.com>
2024-03-20 21:36:41 +0530

    working on json server on diff rep

--- frontend/src/Pages/Quiz.jsx
+++ frontend/src/Pages/Quiz.jsx
@@ -1,6 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import {
+    Box,Input,Select,Flex,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Heading,
+    Button,
+  } from '@chakra-ui/react'
 export default function Quiz(){
-    return{
-
-    }
+    return(
+<Box>
+    <Button></Button>
+    <Box></Box>
+</Box>
+    )
 }
